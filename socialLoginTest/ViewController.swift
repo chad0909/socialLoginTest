71bad9e37f11680f477cafa8efecbfe4839ec177
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2022-11-23 16:43:16 +0900

    Google Login Made

--- socialLoginTest/ViewController.swift
+++ socialLoginTest/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import GoogleSignIn
+
 
 class ViewController: UIViewController {
 
+    @IBAction func googleLogin(_ sender: Any) {
+        GIDSignIn.sharedInstance()?.signIn()
+    }
+    @IBAction func googleLogout(_ sender: Any) {
+        GIDSignIn.sharedInstance()?.signOut()
+    }
+    @IBOutlet weak var id: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var name: UILabel!
+    
+    @IBAction func getInfomation(_ sender: Any) {
+        let user = AppDelegate.user
+        
+        self.id.text = user?.userID
+        self.email.text = user?.profile.email
+        self.name.text = user?.profile.name
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        
     }
 
 
+    
 }
+
 
